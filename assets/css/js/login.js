d4d14f89765657444b21ecbc309e297e3a44dd11
herick <herickrossato@gmail.com>
2020-09-22 15:39:52 -0300

    tela de login finalizada

--- assets/css/js/login.js
+++
@@ -1,8 +0,0 @@
-tentarLogin(){
-    var email = document.querySelector('.email');
-    var senha = document.querySelector('.password');
-
-    if(senha === undefined || email === undefined ){
-        alert("Preencha todos os campos!");
-    }
-}
---
+++ assets/js/login.js
@@ -0,0 +1,62 @@
+var token;
+ativaEnter();
+function tentarLogin(){
+    var emailInput = document.querySelector('#email');
+    var senhaInput = document.querySelector('#senha');
+    var email = emailInput.value;
+    var senha = senhaInput.value;
+
+    if(senha === undefined || email === undefined ){
+        alert("Preencha todos os campos!");
+        return;
+    }
+    desativaLogin();
+    enviaRequisicao(email,senha);
+}
+
+function enviaRequisicao(email,senha){
+    var dados = {
+        "email": email,
+        "password": senha
+    };
+    var requisicao = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/users', dados);
+
+    requisicao.then(processarSucesso).catch(processarErro);
+    
+}
+
+function processarSucesso(sucesso){
+   console.log(sucesso);
+   token = sucesso.data.token;
+}
+
+function processarErro(erro){
+    alert(erro.response.data.message);
+    ativaLogin();
+}
+
+function desativaLogin(){
+    var botao = document.querySelector('.entrar-login');
+    botao.setAttribute('onclick','');
+}
+function ativaLogin(){
+    var botao = document.querySelector('.entrar-login');
+    botao.setAttribute('onclick','tentarLogin()'); 
+}
+
+function ativaEnter(){
+    document.querySelector("#senha").addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.querySelector('.entrar-login').click();
+        }
+    
+    });
+    document.querySelector("#email").addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.querySelector('.entrar-login').click();
+        }
+    
+    });
+}
