60fbc7a1216a734bd06f46f80e3ef079075fb558
herick <herickrossato@gmail.com>
2020-09-23 19:04:27 -0300

    botao adiciona novas perguntas e niveis

--- assets/js/criacaoQuizz.js
+++ assets/js/criacaoQuizz.js
@@ -2,3 +2,21 @@ function carregaCriacaoQuizz(){
     document.querySelector('.tela-criacao-quizz').classList.toggle('esconde-tela');
     document.querySelector('.tela-lista-quizz').classList.toggle('esconde-tela');
 }
+var contPergunta = 1;
+function adicionaPergunta(){
+    var pergunta = document.querySelector(".pergunta-quizz");
+    var novaPergunta = pergunta.cloneNode(true);
+    var tela = document.querySelector(".conteudo-quizz");
+    contPergunta++;
+    tela.appendChild(novaPergunta);
+    novaPergunta.querySelector("h3").innerText = "Pergunta " + contPergunta;
+}
+var contNivel = 1;
+function adicionaNivel(){
+    var nivel = document.querySelector(".caixa-nivel");
+    var novoNivel = nivel.cloneNode(true);
+    var tela = document.querySelector(".nivel-quizz");
+    contNivel++;
+    tela.appendChild(novoNivel);
+    novoNivel.querySelector("h3").innerText = "Nível " + contNivel;
+}
